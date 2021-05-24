1ac92ddc5b832d3c7d98d0443ee1540bd3811963
Anon Ymus <beaver700nh@gmail.com>
2021-05-23 20:42:34 -0400

    main framework basically finished, starting equasol

--- SNSErr.js
+++ SNSErr.js
@@ -5,55 +5,64 @@ const ejs       = require("ejs");
 const stat2desc = require("http-status");
 
 class SNSError extends Error {
-	constructor(status, info) {
-		super();
+  constructor(status, info) {
+    super();
 
-		this.name = status.toString();
-		this.message = info;
-		this.desc = stat2desc[this.status];
-	}
+    this.name = status.toString();
+    this.message = info;
+    this.desc = stat2desc[this.status];
+  }
 
-	get status() {
-		return this.name;
-	}
+  get status() {
+    return this.name;
+  }
 
-	get info() {
-		return this.message;
-	}
+  get info() {
+    return this.message;
+  }
 
-	toString() {
-		return `Error ${this.status} (${this.desc}): ${this.message}`;
-	}
+  toString() {
+    return `Error ${this.status} (${this.desc}): ${this.message}`;
+  }
 }
 
 function show_error(req, res, e) {
-	ejs.renderFile(
-		path.join(consts.PUBLIC_DIR, "error/error.html"),
-		{status: e.status, desc: e.desc, info: e.info},
-		(err, str) => {
-			if (err) {
-				if (status === 508) {
-					res.status(508);
-					res.setHeader("Content-Type", "text/html");
-					res.send(consts.HTML508);
-				}
-				else if (status === 500) {
-					show_error(
-						req, res, new SNSError(508, "Failed to report an error reporting failure.")
-					);
-				}
-				else {
-					show_error(
-						req, res, new SNSError(500, `Failed to report error ${e.toString()}.`)
-					);
-				}
-			}
-			else {
-				res.status(e.status).send(str);
-			}
-		}
-	);
+  ejs.renderFile(
+    path.join(consts.PUBLIC_DIR, "error/error.html"),
+    {status: e.status, desc: e.desc, info: e.info},
+    (err, str) => {
+      if (err) {
+        if (status === 508) {
+          res.status(508);
+          res.setHeader("Content-Type", "text/html");
+          res.send(consts.HTML508);
+        }
+        else if (status === 500) {
+          show_error(
+            req, res, new SNSError(508, "Failed to report an error reporting failure.")
+          );
+        }
+        else {
+          show_error(
+            req, res, new SNSError(500, `Failed to report error ${e.toString()}.`)
+          );
+        }
+      }
+      else {
+        res.status(e.status).send(str);
+      }
+    }
+  );
+}
+
+function on_send_error(req, res, e) {
+  return (err) => {
+    if (err) {
+      show_error(req, res, e);
+    }
+  }
 }
 
 exports.SNSError = SNSError;
 exports.show_error = show_error;
+exports.on_send_error = on_send_error;
--- index.js
+++ index.js
@@ -8,24 +8,48 @@ const app     = express();
 
 app.use(express.static(consts.PUBLIC_DIR));
 
+app.use(
+  (req, res, next) => {
+    console.log(`GOT A REQUEST for url ${req.url}`);
+    next();
+  }
+);
+
 app.get(
-	"/",
-	(req, res) => {
-		res.sendFile(path.join(consts.PUBLIC_DIR, "index/index.html"));
-	}
+  "/",
+  (req, res) => {
+    res.sendFile(
+      path.join(consts.PUBLIC_DIR, "index/index.html"),
+      snserr.on_send_error(
+        req, res, new snserr.SNSError(500, "The server failed to find the main page.")
+      )
+    );
+  }
+);
+
+app.get(
+  "/equasol",
+  (req, res) => {
+    res.sendFile(
+      path.join(consts.PUBLIC_DIR, "equasol/equasol.html"),
+      snserr.on_send_error(
+        req, res, new snserr.SNSError(500, "The server failed to find EquaSol.")
+      )
+    );
+  }
 );
 
 app.listen(
-	consts.PORT,
-	() => {
-		console.log(`StuffNStuff server is up and running on port ${consts.PORT}`);
-	}
+  consts.PORT,
+  () => {
+    console.log(`StuffNStuff server is up and running on port ${consts.PORT}.`);
+  }
 );
 
 app.use( // No routes - assume error 404
-	(req, res) => {
-		snserr.show_error(
-			req, res, new snserr.SNSError(404, "The server could not find the requested file.")
-		);
-	}
+  (req, res) => {
+    snserr.show_error(
+      req, res, new snserr.SNSError(404, "The server could not find the requested file.")
+    );
+  }
 );
