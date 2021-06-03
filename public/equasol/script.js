f814b620b0372a0597b910ee230b9a184c1691a0
Anon Ymus <>
2021-06-03 14:36:47 +0000

    moved/cleaned equasol

--- public/equasol/script.js
+++ public/equasol/script.js
@@ -1,48 +1,6 @@
 $(document).ready(
   () => {
-    $("#xprod-calc").click(
-      function () {
-        let values = [
-          parseInt($("#xprod-n1").val()),
-          parseInt($("#xprod-d1").val()),
-          parseInt($("#xprod-n2").val()),
-          parseInt($("#xprod-d2").val()),
-        ];
-
-        if (check("0011", values)) {
-          XPROD.simplify(values[2], values[3], "#xprod-n1", "#xprod-d1", "#xprod-status");
-        }
-        else if (check("0111", values)) {
-          XPROD.solve_lr(values[1], values[2], values[3], "#xprod-n1", "#xprod-status");
-        }
-        else if (check("1011", values)) {
-          XPROD.solve_ur(values[0], values[2], values[3], "#xprod-d1", "#xprod-status");
-        }
-        else if (check("1100", values)) {
-          XPROD.simplify(values[0], values[1], "#xprod-n2", "#xprod-d2", "#xprod-status");
-        }
-        else if (check("1101", values)) {
-          XPROD.solve_ll(values[0], values[1], values[3], "#xprod-n2", "#xprod-status");
-        }
-        else if (check("1110", values)) {
-          XPROD.solve_ul(values[0], values[1], values[2], "#xprod-d2", "#xprod-status");
-        }
-        else if (check("1111", values)) {
-          $("#xprod-status").text("Error: proportion is already solved.");
-        }
-        else {
-          $("#xprod-status").text("Error: not enough information.");
-        }
-      }
-    );
-
-    $("#xprod-clear").click(
-      function () {
-        $("#xprod-n1").val("");
-        $("#xprod-d1").val("");
-        $("#xprod-n2").val("");
-        $("#xprod-d2").val("");
-      }
-    );
+    $("#xprod-calc").click(XPROD.calculate);
+    $("#xprod-clear").click(XPROD.clear);
   }
 );
--- public/equasol/xprod_funcs.js
+++ public/equasol/xprod_funcs.js
@@ -1,7 +1,43 @@
 class XPROD {
-  static simplify_frac(n, d) {
-    let f = gcf(n, d);
-    return [n / f, d / f];
+  static calculate() {
+    let values = [
+      parseInt($("#xprod-n1").val()),
+      parseInt($("#xprod-d1").val()),
+      parseInt($("#xprod-n2").val()),
+      parseInt($("#xprod-d2").val()),
+    ];
+
+    if (check("0011", values)) {
+      XPROD.simplify(values[2], values[3], "#xprod-n1", "#xprod-d1", "#xprod-status");
+    }
+    else if (check("0111", values)) {
+      XPROD.solve_lr(values[1], values[2], values[3], "#xprod-n1", "#xprod-status");
+    }
+    else if (check("1011", values)) {
+      XPROD.solve_ur(values[0], values[2], values[3], "#xprod-d1", "#xprod-status");
+    }
+    else if (check("1100", values)) {
+      XPROD.simplify(values[0], values[1], "#xprod-n2", "#xprod-d2", "#xprod-status");
+    }
+    else if (check("1101", values)) {
+      XPROD.solve_ll(values[0], values[1], values[3], "#xprod-n2", "#xprod-status");
+    }
+    else if (check("1110", values)) {
+      XPROD.solve_ul(values[0], values[1], values[2], "#xprod-d2", "#xprod-status");
+    }
+    else if (check("1111", values)) {
+      $("#xprod-status").text("Error: proportion is already solved.");
+    }
+    else {
+      $("#xprod-status").text("Error: not enough information.");
+    }
+  }
+
+  static clear() {
+    $("#xprod-n1").val("");
+    $("#xprod-d1").val("");
+    $("#xprod-n2").val("");
+    $("#xprod-d2").val("");
   }
 
   static simplify(n_in, d_in, n_out, d_out, status) {
@@ -16,6 +52,11 @@ class XPROD {
 
       $(status).text("Success: fraction has been simplified.");
     }
+  }
+
+  static simplify_frac(n, d) {
+    let f = gcf(n, d);
+    return [n / f, d / f];
   }
 
   static solve_ul(a, b, c, out, status) {
